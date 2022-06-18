f58e55c3568dda7f50fc6c175035a5d893af3ec4
Murodulla <yokubjanovich@gmail.com>
2022-06-18 16:02:44 +0500

    uhshadiyov

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-let numbersOfSeries = 
+let numbersOfSeries = prompt("Nechta serial ko’rdingiz " + " ");
+const seriesDB = {
+  count: numbersOfSeries,
+  series: { kurgan, soni },
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+for(let i = 1; i<=2; i++ ){
+    let kurgan = prompt( "Oxirgi ko'rgan serialingiz?");
+    let soni = prompt("Nechchi baho berasiz?")
+    return;
+}
+
+console.log(seriesDB);
