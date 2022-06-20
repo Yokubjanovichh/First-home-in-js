3ac0adb081203f76a44749fcc02f500f691ab642
Murodulla <yokubjanovich@gmail.com>
2022-06-20 18:45:09 +0500

    yangilanish

--- app.js
+++ app.js
@@ -8,14 +8,24 @@ const seriesDB = {
   privat: false,
 };
 
-let a = prompt("Oxirgi ko'rgan serialingiz?", ""),
-  b = prompt("Nechchi baho berasiz?", ""),
-  c = prompt("Oxirgi ko'rgan serialingiz?", ""),
-  d = prompt("Nechchi baho berasiz?", "");
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Oxirgi ko'rgan serialingiz?", ""),
+    b = prompt("Nechchi baho berasiz?", "");
+  if (a != null && a != "" && b != null && b != "") {
+    seriesDB.series[a] = b;
+    console.log("Done");
+  } else {
+    console.log("ERROR");
+    --i;
+  }
+}
 
-  seriesDB.series[a]=b;
-  seriesDB.series[c] = d;
+if (seriesDB.count <= 5) {
+   console.log("Kam serial ko'ripsiz");
+} else if (5 < seriesDB.count <= 10) {
+   console.log( "Siz classik tamoshabin ekansiz");
+} else {
+   console.log( "Siz serialchi zvezda ekansiz");
+}
 
-  
-  console.log(seriesDB);
-  
+console.log(seriesDB);
