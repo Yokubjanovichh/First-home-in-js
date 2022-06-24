b0d2558305e71027756b146f62d35e9f34547473
Murodulla <yokubjanovich@gmail.com>
2022-06-24 17:21:33 +0500

    yangiladim

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-let numbersOfSeries = prompt("Nechta serial ko'rdingiz ", "");
+let numbersOfSeries;
+let b;
+
+function startApp() {
+  numbersOfSeries = prompt("Nechta serial ko'rdingiz ", "");
+  while (
+    numbersOfSeries == "" ||
+    numbersOfSeries == null ||
+    isNaN(numbersOfSeries)
+  ) {
+    numbersOfSeries = prompt("Nechta serial ko'rdingiz ", "");
+  }
+}
+
+startApp();
 
 const seriesDB = {
   count: numbersOfSeries,
@@ -8,24 +22,52 @@ const seriesDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  let a = prompt("Oxirgi ko'rgan serialingiz?", ""),
+function rememberMySeries() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Oxirgi ko'rgan serialingiz?", "");
     b = prompt("Nechchi baho berasiz?", "");
-  if (a != null && a != "" && b != null && b != "") {
-    seriesDB.series[a] = b;
-    console.log("Done");
-  } else {
-    console.log("ERROR");
-    --i;
+    while (b == "" || b == null || isNaN(b)) {
+      b = prompt("Nechchi baho berasiz?", "");
+    }
+    if (a != null && a != "" && b != null && b != "") {
+      seriesDB.series[a] = b;
+      console.log("Done");
+    } else {
+      console.log("ERROR");
+      --i;
+    }
   }
 }
 
-if (seriesDB.count <= 5) {
-   console.log("Kam serial ko'ripsiz");
-} else if (5 < seriesDB.count <= 10) {
-   console.log( "Siz classik tamoshabin ekansiz");
-} else {
-   console.log( "Siz serialchi zvezda ekansiz");
+rememberMySeries();
+
+function detectLevelSeries(miqdor) {
+  if (miqdor <= 5) {
+    console.log("Kam serial ko'ripsiz");
+  } else if (5 < miqdor <= 10) {
+    console.log("Siz classik tamoshabin ekansiz");
+  } else {
+    console.log("Siz serialchi zvezda ekansiz");
+  }
 }
 
-console.log(seriesDB);
+detectLevelSeries(seriesDB.count);
+
+function writeGenres() {
+  for (let i = 0; i <= 2; i++) {
+    let sevganJanrim = prompt(`Yaxshi ko'rgan janringiz ${i + 1}`, "");
+    seriesDB.genres[i] = sevganJanrim;
+  }
+}
+
+writeGenres();
+
+function showDb(yashirin) {
+  if (yashirin == false) {
+    console.log(seriesDB);
+  } else {
+    console.log("xatolik yuz berdi");
+  }
+}
+
+showDb(seriesDB.privat);
